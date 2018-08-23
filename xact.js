61c13e9bf279a3d5a75993ae344c0a0cb1bf0932
tlack <lackner@gmail.com>
2018-08-23 11:31:39 -0400

    misc

--- xact.js
+++ xact.js
@@ -1,22 +1,36 @@
+// TODO use better array intrinsics
 function xact(Scope) {
 	if(!Scope) Scope=this;
 	Scope.SHOWASSERT=SHOWASSERT=1;
 	Scope.MAXITER=MAXITER=5;
 	Scope.U=U=typeof(blehhhh);
 
+	// Values: Make one..
+	function make(x, newtype) {
+		if(!tstr(newtype)) throw 'make(): newtype::str'; // TODO declarative expects
+		if(newtype[0]=='$') return [newtype,x];
+		throw 'make(): nyi';
+	}
+	Scope.make=make;
+
 	// Values: Misc..
-	function copy(x) { return jd(je(x)); }
-	Scope.copy=copy;
+	function amend(x,L,V) {
+		if(!tarray(L)) { L=[L]; if (!tfunc(V)) V=[V]; }
+		var i;
+		if(tfunc(V)) for(i=0;i<L.length;i++) x[L[i]]=V(x[L[i]],i);
+		else         for(i=0;i<L.length;i++) x[L[i]]=V[i];
+		return x;
+	} Scope.amend=amend;
+	function copy(x) { return jd(je(x)); } Scope.copy=copy;
 	function eq(x,y) { //emit([x,y],'equal');
 		if (typeof(x)!==typeof(y)) return false;
 		if (Array.isArray(x) && Array.isArray(y) && x.length!=y.length) return false;
 		if (typeof(x)!=='object' && x==y) return true; //simple values - hopefully
-		return JSON.stringify(x)==JSON.stringify(y); }
-	Scope.eq=eq;
+		return JSON.stringify(x)==JSON.stringify(y); } Scope.eq=eq;
+	function empty(x) { return len(x)===0; } Scope.empty=empty;
 	function first(x) {
 		if(!tarray(x)) throw 'first(): x::array';
-		return x[0]; }
-	Scope.first=first;
+		return x[0]; } Scope.first=first;
 	function ins(x,y) {
 		if(tstr(x)&&tstr(x)) { return x+y; }
 		if(tstr(x)&&tarray(y)&&tstr(y[0])) { return x+over(ins,y) }/*!killme*/
@@ -43,6 +57,13 @@ function xact(Scope) {
 	Scope.max=max;
 	function min(mm,mx) { if(tarray(mm)) return Math.min.apply(null,mm); else return Math.min(mm,mx); }
 	Scope.min=min;
+	function issym(x) { return tarray(x)&&tstr(x[0])&&x[0][0]=='$'?true:false; }
+	Scope.issym=issym;
+	function $sym(x) {
+		if(issym(x)) return x[0];
+		return false;
+	}
+	Scope.$sym=$sym;
 	function take(x,n) {
 		let st,en=n,i,R=[],xn=len(x);
 		if(n<0) st=xn-n; else st=0;
@@ -75,17 +96,12 @@ function xact(Scope) {
 		else if (t==='number') return 'float'; /*lame*/ else if (t==='function') return 'func';
 		return t; /*fallthru*/ }
 	Scope.t=t;
-	function tarray(x) { return Array.isArray(x); }
-	Scope.tarray=tarray;
-	function tstr(x) { return typeof(x)==='string'; }
-	Scope.tstr=tstr;
-	function tbox(x) { return typeof(x)==='object' || Array.isArray(x); }
-	Scope.tbox=tbox;
-	function tdict(x) { return typeof(x)==='object' && !Array.isArray(x); }
-	Scope.tdict=tdict;
-	function tfunc(x) { return typeof(x)==='function'; }
-	Scope.tfunc=tfunc;
-	function tU(x) { return typeof(x)==='undefined'; }
+	function tarray(x) { return Array.isArray(x); } Scope.tarray=tarray;
+	function tstr(x) { return typeof(x)==='string'; } Scope.tstr=tstr;
+	function tbox(x) { return typeof(x)==='object' || Array.isArray(x); } Scope.tbox=tbox;
+	function tdict(x) { return typeof(x)==='object' && !Array.isArray(x); } Scope.tdict=tdict;
+	function tfunc(x) { return typeof(x)==='function'; } Scope.tfunc=tfunc;
+	function tU(x) { return x===undefined; } Scope.tU=tU;
 	// Debugging:
 	Scope._req=function _req(fs) {
 		if(typeof require=='undefined') throw('_req(): cannot get '+fs);
@@ -109,7 +125,7 @@ function xact(Scope) {
 	function je(x) { return JSON.stringify(x); }
 	Scope.je=je;
 	// More trippy stuff
-	function each(x,f) { x.forEach(f); }
+	function each(x,f) { var R=[]; for(var i=0;i<x.length;i++) R.push(f(x[i],i)); return R; }
 	Scope.each=each;
 	function eachleft(x,f) {
 		if(!tarray(x) || x.length != 2 || !tarray(x[0])) throw 'eachLeft(): x must be [ [1, 2, 3], 10 ]';
@@ -139,10 +155,8 @@ function xact(Scope) {
 		return val;
 	}
 	Scope.over=over;
-	function projleft(f,x) { return function(y) { f(x,y); } }
-	Scope.projleft=projleft;
-	function projright(f,x) { return function(y) { f(x,y); } }
-	Scope.projright=projright;
+	function projleft(f,x)  {return function(y) {return f(x,y);}} Scope.projleft=projleft;
+	function projright(f,y) {return function(x) {return f(x,y);}} Scope.projright=projright;
 	function scan(x,f,val) { // binary function f; [ f(x[0],x[1]), f(f(x[0],x[1]),x[2]), ... ]
 		if(!tarray(x)) throw 'scan(): x must be array';
 		if(tU(val)) val=x.shift();
@@ -164,7 +178,9 @@ function xact(Scope) {
 		if(path==undefined) path=[];
 		let R=[];
 		for (let i=0;i<xl;i++) {
-			if(tarray(x[i])) { var p=ins(path,i); last=wide(f(x[i],p,last),f,last,p); if(!tU(last)) R.push(last); }
+			if(tarray(x[i])) { 
+				var p=ins(path,i); last=wide(f(x[i],last,p),f,last,p); 
+				if(!tU(last)) R.push(last); }
 			else R.push(x[i]);
 		}
 		return R;
@@ -172,13 +188,15 @@ function xact(Scope) {
 		//[ [ 7, 2, [ 21, 4 ] ], 8 ]
 	}
 	Scope.wide=wide;
-	function deep(x,f,last) { 
+	function deep(x,f,last,path) { 
 		if(!tarray(x)) throw 'deep(): arg not list';
 		let xl=x.length; if(xl==0) return x;
 		if(last==undefined) last=x;
+		if(path==undefined) path=[];
 		let R=[];
 		for (let i=0;i<xl;i++) {
-			if(tarray(x[i])) last=deep(x[i],f,last); else last=f(x[i],i,last);
+			var p=ins(path,i); 
+			if(tarray(x[i])) last=deep(x[i],f,last,p); else last=f(x[i],last,p);
 			if(!tU(last)) R.push(last);
 		}
 		return R;
@@ -197,16 +215,87 @@ function xact(Scope) {
 		} else return x[idx]; // todo funcs
 	}
 	Scope.get=get;
-	function match(x,sym) {
+	function match(x,patterns) {
 		if(!tarray(x)) throw 'match(): x must be [ ["$syma"],["$symb",3,4,5] ]';
+		emit(x,'match x');
+		emit(patterns,'match pat');
 		var R=[];
-		function visit(x, path, last) { if(x[0]==sym) R.push(path); return x; }
-		var z=wide(x, visit);
+		function visitSym(x, last, path) { 
+			if(x[0]==patterns) R.push(emit(path,'visitSym')); return x; }
+		function visitVal(x, last, path) { 
+			if(x==patterns) R.push(path); return x; }
+		function visitAnd(x, last, path) {
+			emit(x,'visitAnd x');
+			emit(patterns,'visitAnd pat');
+			let i,j, patn=len(patterns), xn=len(x);
+			for(i=0;i<xn;i++) {
+				emit(i,'i');
+				var matchval=[];
+				var found=1;
+				var xij,pj;
+				for(j=0;j<patn;j++) {
+					emit(j,'j');
+					pj=patterns[j]; xij=x[i+j];
+					if(issym(pj) && $sym(pj) == $sym(xij)) {
+						emit([pj,$sym(pj),xij,$sym(xij)],'visitAnd match');
+						continue;
+					}
+					if(pj == xij) {
+						emit([pj,xij],'visitAnd value match'); continue; }
+					found=0;
+					break;
+				}
+				if (found && j==patn) {
+					var p=ins(path,i);
+					emit(p,'visitAnd adding path');
+					emit(i,'visitAnd i');
+					emit(xij,'visitAnd value xij');
+					R.push(p); // made it this far, pattern matched
+				}
+			}
+			return x; }
+		var z;
+		if(tarray(patterns)) {
+			let op=$sym(patterns); 
+			if(op=='$and') { 
+				patterns=patterns[1]; 
+				z=wide(x,visitAnd);
+			}
+		}
+		if(patterns[0]=='$') z=wide(x,visitSym);
+		else z=deep(x,visitVal);
 		return R;
 	}
 	Scope.match=match;
-	function _nest0(L, open, close, path) {
-		noemit(L,'start _nest0 '+path);
+
+	function resolve(x, patterns) {
+		// patlist like
+		//   [  2, numhandler, 3, numhandler, '$thing', thinghandler, elsehandler.. ]
+		if(!tarray(patterns)) throw 'resolve(): patterns must be [val, func, "$sym", func, elsefunc]';
+		var patn=len(patterns);
+		function _resolve0(x, last, path) {
+			var i;
+			emit(x,'resolve0');
+			for(i=0;i<patn;i+=2) {
+				if(patterns[i][0]=='$' && x[0]==patterns[i] ||
+					 patterns[i]==x ||
+					 (patterns[i].test && patterns[i].test(x))) {
+					emit(patterns[i],'resolve sym match');
+					var fn=patterns[i+1];
+					if(tfunc(fn)) x=fn(x); else x=fn;
+					emit(x,'amended');
+					continue;
+				}
+			}
+			return x;
+		}
+		var z=deep(x, _resolve0);
+		return z;
+	}
+	Scope.resolve=resolve;
+
+	function _nest0(L, open, close, cb, path) {
+		emit(L,'start _nest0 '+path);
 		const LL=L.length; var opens=[],R=[],i;
 		if(LL==0) return L;
 		R=L;
@@ -216,28 +305,57 @@ function xact(Scope) {
 				var oidx=opens.pop();
 				var inner=R.slice(oidx+1, i); 
 				emit(R,'presplice');
+				if(cb) { inner=emit(cb(inner),'nest-inner-cb'); }
 				R.splice(oidx, i-oidx+len(open)+len(close)-1, inner);
 				emit(R,'recurse');
-				return _nest0(R, open, close, ins(path,i));
+				return _nest0(R, open, close, cb, ins(path,i));
 				emit([oidx,i],'replacing');
 				//emit(ins(path,i),'_nest0 path');
 			} else {
 				if(L[i]==open) opens.push(i);
 			}
 		}
-		noemit(R,'_end nest0 '+path);
+		emit(R,'_end nest0 '+path);
 		return R;
 	}
-	function nest(L, open, close) {
+	function _nest1(L, open, close, cb, path) {
+		emit(L,'start _nest1 '+path);
+		const LL=L.length; var opens=[],R=[],i;
+if(LL==0) return L;
+		R=L;
+		for(i=0; i<LL; i++) {
+			if(L[i]==open) {
+				if(len(opens)==0) { opens.push(i); continue; }
+				emit(i,'found close');
+				if (opens.length==0) throw 'nest(): imbalanced';
+				var oidx=opens.pop();
+				var inner=R.slice(oidx+1, i); 
+				emit(R,'presplice');
+				if(cb) { inner=emit(cb(inner),'nest-inner-cb'); }
+				R.splice(oidx, i-oidx+len(open)+len(close)-1, inner);
+				emit(R,'recurse');
+				return _nest1(R, open, close, cb, ins(path,i));
+				emit([oidx,i],'replacing');
+				//emit(ins(path,i),'_nest0 path');
+			}
+		}
+		emit(R,'_end nest1 '+path);
+		return R;
+	}
+	function nest(L, open, close, handlecb) {
 		if(!tarray(L)) throw('nest(): L::array');
 		L=[L];
 		emit(L,"nest");
-		R=wide(L,function(x,path){return _nest0(x,open,close,path);});
-		//return _nest0(L,open,close,0);
+		var nestfn=(open == close)?_nest1 : _nest0;
+		R=wide(L,function(x,last,path){return nestfn(x,open,close,handlecb,path);});
 		return R;
 	}
 	Scope.nest=nest;
 	Scope.selftest=function() {
+
+		assert($sym(make(1,'$z')),'$z','sym0');
+		assert(issym(make(2,'$bbbb')),true,'sym1');
+
 		assert(asc([4,1,7]),[1,4,7],'asc0');
 		assert(desc([4,1,7]),[7,4,1],'desc0');
 		assert(take([1,2,3],1),[1],'take0');
@@ -260,7 +378,7 @@ function xact(Scope) {
 		var b=wide(t,function(x){emit(x,'wide-b-x'); if (!tarray(x[0])) x[0]=x[0]*7;return x;});
 		emit(b,'finalb');
 		assert(last(b),8,'wide0'); assert(b[0][0],7,'wide1'); assert(b[0][2][0],21,'wide2');
-		var t_sym=[ ['$name','Bob'], ['$age',30], ['$parents',['$name','Olga'],['$name','Sam']] ];
+		var t_sym=[ ['$name','Bob'], make(30,'$age'), ['$parents',['$name','Olga'],make('Sam','$name')]];
 		var c=wide(t_sym,function(x){if(first(x)=='$name')x[1]=x[1].toUpperCase();return x;});
 		assert(first(c)[1],'BOB','wide0sym');
 
@@ -270,6 +388,10 @@ function xact(Scope) {
 		var d=match(t_sym, '$name');
 		var e=eachright([t_sym,d],get);
 		assert(len(e),len(d),'match-er-0');
+
+		emit(d);
+		emit(e);
+
 		assert(first(last(e)),'$name','match-er-1');
 
 		o=nest("1(2)".split(''),"(",")");
@@ -292,10 +414,36 @@ function xact(Scope) {
 		flatten=function(x) { return wide(x, function(x){return x.join('');}); }
 		o2=flatten(o);
 		emit(je(o2));
-		//o3=wide(o[0],ins);
-		//emit(o3);
-		//o=wide(["(1 (2) (3) ) ".split('')],function(x){return nest(x.join(''),"(",")");});
-		//emit(o);
+		var q=[1, 2, [3, 4]];
+		amend(q,0,5);
+		assert(q[0],5,'amend0');
+		qq=amend(q,[0,1],[8,9]);
+		assert(qq,[8,9,[3,4]],'amend1');
+		qqq=amend(q,2,function(x){return [x[0]*2,x[1]*3]});
+		assert(qqq,[8,9,[6,12]],'amend2');
+
+		var d=[1], p=[1], r=match(d,p);
+		assert(r,[[0]],'m1-t1');
+		var d=[1], p=['$and',[1]], r=match(d,p);
+		assert(r,[[0]],'m1-t2');
+		var d=[1], p=['$and',[1,2]], r=match(d,p);
+		assert(r,[],'m1-t3');
+		var d=[[1,2]], p=['$and',[1,2]], r=match(d,p);
+		assert(r,[[0,0]],'m1-t4');
+		var d=[1,2], p=['$and',[1]], r=match(d,p);
+		assert(r,[[0]],'m1-t5');
+
+		var md=[[1,2]];
+		var m1=match(md,make([1,2],'$and'));
+		assert(m1,[[0,0]],'match AND 1');
+		var m1=match(md,make([1,2,1],'$and'));
+		assert(m1,[],'match AND 2');
+		assert(match([[1,2,1]],['$and',[1,2]]),[[0,0]],'match AND 3');
+		assert(match([[1,2,1]],['$and',[2,1]]),[[0,1]],'match AND 4');
+		assert(match([[1,2,1]],['$and',[1,2,1]]),[[0,0]],'match AND 5');
+
+		var d=[[1,2,1]], p=['$and',[1]], r=match(d,p);
+		emit(r,'r');
 		emit('all tests','passed');
 	}
 	return Scope;
@@ -304,3 +452,4 @@ function xact(Scope) {
 if(typeof(module)!='undefined') {
 	module.exports = xact; 
 }
+
